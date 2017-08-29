2c8052bce3a426d00aba195067f172721606512f
devilsega <devilsega@yandex.ru>
2017-08-29 16:38:11 +0300

    Added CORS support.

--- src/main/java/ua/usstandart/avayastatformer/controller/RestController.java
+++ src/main/java/ua/usstandart/avayastatformer/controller/RestController.java
@@ -13,6 +13,7 @@ public class RestController {
     @Autowired
     StatsFormer statsFormer;
 
+    @CrossOrigin
     @RequestMapping(method= RequestMethod.GET, value="/api/stat/1.0/get")
     public Object getStats (@RequestParam(value = "number")String number, @RequestParam(value = "heading")String heading, @RequestParam(value = "date") @DateTimeFormat(pattern="yyyy-MM-dd") Date date){
         return statsFormer.getStats(number,heading,date);
